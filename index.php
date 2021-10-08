68120ab070c1dbbb2531e8a6ae6c8231c76229e5
BigFelk <fedferrara89@gmail.com>
2021-10-08 13:47:51 +0200

    snak 1 fatto

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>snacks</title>
+</head>
+<body>
+    <?php
+     $partite = [
+        [
+            "squadra1" => "armani",
+            "squadra2" => "cantu",
+            "punteggio1"=> 50,
+            "punteggio2"=> 60,
+        ],
+
+        [
+            "squadra1" => "monaco",
+            "squadra2" => "zenit",
+            "punteggio1"=> 65,
+            "punteggio2"=> 70,
+        ],
+        [
+            "squadra1" => "cska",
+            "squadra2" => "stella rossa",
+            "punteggio1"=> 40,
+            "punteggio2"=> 100
+        ]
+        ];
+        for ($i=0; $i< count($partite); $i++){
+            echo "<div>".$partite[$i][squadra1]." - " .  $partite[$i][squadra2] . " | " .$partite[$i][punteggio1]."-".$partite[$i][punteggio1]."</div>" ;  
+        }
+    
+
+
+
+    ?>
+</body>
+</html>
